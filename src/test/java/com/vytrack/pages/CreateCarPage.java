baec312ca470bd3b9479e787ed3a00049d48a9be
slokostic3212 <dsk3212@yahoo.com>
2020-10-25 12:07:19 -0400

    update BrowserUtils

--- src/test/java/com/vytrack/pages/CreateCarPage.java
+++ src/test/java/com/vytrack/pages/CreateCarPage.java
@@ -31,4 +31,4 @@ public class CreateCarPage extends BasePage {
     }
 
     }
-}
+
--- src/test/java/com/vytrack/utils/BrowserUtils.java
+++ src/test/java/com/vytrack/utils/BrowserUtils.java
@@ -2,10 +2,11 @@ package com.vytrack.utils;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BrowerUtils {
+public class BrowserUtils {
     private static WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
 
     public static void wait(int seconds){
@@ -24,7 +25,27 @@ public class BrowerUtils {
         wait.until(ExpectedConditions.visibilityOf(element));
         element.clear();
         element.sendKeys(text);
+        wait.until(ExpectedConditions.attributeToBe(element,"value", text));
         System.out.println("Entering text: " + text);
 
     }
+    /**
+     *
+     *      * For instance, selenium webdriver is trying to enter the text:
+     *      * Expected text input  actual text input
+     *      *
+     *      * [java is great] --> [java is g]
+     *      *
+     *      * for some reason, selenium jumps to the next step without finishing text input here
+     *      *
+     *      * to prevent this issue, we need to make it wait until text is completely entered.
+     *      *
+     *      * How to do that?
+     *      *
+     *      * As we know, text is stored in the value attribute. So we need to wait until value attribute
+     *      * of the element will obtain expected text.
+     *
+     */
+
+
 }
