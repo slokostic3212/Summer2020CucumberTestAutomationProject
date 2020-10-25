2f46461896c94cbc5e4a9a57107413320bbbbd2a
slokostic3212 <dsk3212@yahoo.com>
2020-10-25 17:10:06 -0400

    update pages

--- src/test/java/com/vytrack/pages/CreateCalendarEventPage.java
+++ src/test/java/com/vytrack/pages/CreateCalendarEventPage.java
@@ -2,40 +2,48 @@ package com.vytrack.pages;
 
 import com.vytrack.utils.BrowserUtils;
 import com.vytrack.utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CreateCalendarEventPage extends BasePage{
+public class CreateCalendarEventPage extends BasePage {
 
+    @FindBy(css = "[title='Create Calendar event']")
     private WebElement createCalendarEventBtn;
 
-    @FindBy(css = "[title='Create Calendar Event']")
-    private WebElement getCreateCalendarEventBtn;
-
-    @FindBy(name = "or_calendar_event_form[title]")
+    @FindBy(name = "oro_calendar_event_form[title]")
     private WebElement titleInputBox;
 
     @FindBy(id = "tinymce")
     private WebElement descriptionInputBox;
 
-    public void enterTitle(String text){
+    public void enterTitle(String text) {
         BrowserUtils.enterText(titleInputBox, text);
     }
-    public void clickOnCreateCalendarEvent(){
+
+    public void clickOnCreateCalendarEvent() {
         BrowserUtils.clickOnElement(createCalendarEventBtn);
     }
 
-    public void enterDescriptoin(String text){
+    public void enterDescription(String text) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
-        //exit all frames
+        //exit from all frames
         Driver.getDriver().switchTo().defaultContent();
-        //wait for frame ans switch to it
+        //wait for frame and switch to it
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
         //enter text
-        BrowserUtils.enterText(descriptionInputBox, text);
-        //exit from frame
+//        BrowserUtils.enterText(descriptionInputBox, text);
+        descriptionInputBox.sendKeys(text);
+        //exit from the frame
         Driver.getDriver().switchTo().defaultContent();
+    }
+
+    public String getDataFromGeneralInfo(String parameterName) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        String xpath = "//label[text()='" + parameterName + "']/../div/div";
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        return element.getText().trim();
     }
 }
--- src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
@@ -2,7 +2,9 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.pages.CreateCalendarEventPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import java.util.Map;
 
@@ -11,7 +13,7 @@ public class CreateCalendarEventStepDefinitions {
 
     @Given("user clicks on create calendar event button")
     public void user_clicks_on_create_calendar_event_button() {
-
+        createCalendarEventPage.clickOnCreateCalendarEvent();
     }
 
     /*
@@ -23,7 +25,17 @@ public class CreateCalendarEventStepDefinitions {
         String title = data.get("Title");
         String description = data.get("Description");
 
-        createCalendarEventPage.enterDescriptoin(description);
+        createCalendarEventPage.enterDescription(description);
         createCalendarEventPage.enterTitle(title);
+    }
+
+    @Then("user verifies that new calendar event is displayed:")
+    public void user_verifies_that_new_calendar_event_is_displayed(Map<String, String> data) {
+        String title = data.get("Title");
+        String description = data.get("Description");
+
+        Assert.assertEquals(title, createCalendarEventPage.getDataFromGeneralInfo("Title"));
+        Assert.assertEquals(description, createCalendarEventPage.getDataFromGeneralInfo("Description"));
+
     }
 }
